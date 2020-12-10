d3119b57258cb5549025a63c1ab86226aa146c2f
fanta <gcidart@gmail.com>
2020-12-10 10:49:25 +0530

    day10

---
+++ day10/Main.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end() 
+#define tr(c,i) for(auto i = (c).begin(); i != (c).end(); i++) 
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define F(i,n) for(int i=0;i<n;i++)
+#define MOD 1000000007
+#define VE(i,v) for(int i = 0;i < (v).size();i++)
+
+using namespace std;
+#define trace(...) cerr<<__FUNCTION__<<":"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+	cerr<<name<<" : "<<arg1<<endl;
+}
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+	const char *comma=strchr(names+1,',');
+	cerr.write(names,comma-names)<<" : "<<arg1<<" | ";
+	__f(comma+1,args...);
+}
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef unsigned int ui;
+typedef vector<ll> vl; 
+typedef vector<vl> vvl; 
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpll; 
+#define MOD2 1LL<<32
+
+
+	
+int main()
+
+{
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int T=1;
+	//cin>>T;
+	
+	
+	
+	for(int tc=1; tc<=T;tc++)
+	{
+	    int n;
+        cin>>n;
+        vi arr(n);
+        for(int i =0; i<n; i++)
+            cin>>arr[i];
+        sort(all(arr));
+        int diff1 = 0;
+        int diff3 = 0;
+        if(arr[0]==1)
+            diff1++;
+        else if(arr[0]==3)
+            diff3++;
+        for(int i =1; i<n; i++)
+        {
+            int diff = arr[i] - arr[i-1];
+            if(diff==1)
+                diff1++;
+            else if(diff==3)
+                diff3++;
+        }
+        diff3++;
+        trace(diff1*diff3);
+        vl dp(n+1);
+        for(int i =0; i<n; i++)
+        {
+            if(arr[i]<=3)
+                dp[i]++;
+        }
+        for(int i =1; i<n; i++)
+        {
+            for(int j = 0; j<i; j++)
+            {
+                if(arr[i]-arr[j]<=3)
+                    dp[i]+= dp[j];
+            }
+        }
+        trace(dp[n-1]);
+
+
+
+
+
+
+
+
+
+		
+		
+
+
+
+
+
+		
+		//cout<<"Case #"<<tc<<": "<<"NO"<<"\n";
+
+	}
+  
+ 
+ 
+	return 0;
+}
+ 
+ 
